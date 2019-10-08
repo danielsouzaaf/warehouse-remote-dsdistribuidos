86ae1311d9b0af2d014dc93730a400e2ca51ad11
Daniel Souza <daniel.souza.af@gmail.com>
2019-10-08 11:37:44 -0300

    finalização do projeto

--- armazem.py
+++ armazem.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
+import Pyro4
 
 
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
 class Armazem(object):
     def __init__(self):
         self.itens = ["cadeira", "bicicleta", "lanterna", "laptop", "almofada"]
@@ -15,3 +18,15 @@ class Armazem(object):
     def guardar(self, nome, item):
         self.itens.append(item)
         print("{0} guardou a(o) {1}.".format(nome, item))
+
+
+def main():
+    Pyro4.Daemon.serveSimple(
+        {
+            Armazem: "example.armazem"
+        },
+        ns=False)
+
+
+if __name__ == "__main__":
+    main()
--- visit.py
+++ visit.py
@@ -1,9 +1,10 @@
 # This is the code that runs this example.
-from armazem import Armazem
 from pessoa import Pessoa
+import Pyro4
 
-armazem = Armazem()
-janet = Pessoa("Janet")
-henry = Pessoa("Henry")
-janet.visitar(armazem)
-henry.visitar(armazem)
+uri = input("Enter the uri of the warehouse: ").strip()
+armazem = Pyro4.Proxy(uri)
+gracon = Pessoa("Gracon")
+daniel = Pessoa("daniel")
+gracon.visitar(armazem)
+daniel.visitar(armazem)
